ca0f9d463021023f0e3f3d2d5fc55e3291af5559
stevenng00 <xiaogeng00@gmail.com>
2024-03-22 22:44:04 +0800

    20240322

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult
+    }
+}
