88b2d4b529212c7f8ff2ed015d8ceb4a79a69f3c
stevenng00 <xiaogeng00@gmail.com>
2024-11-13 17:47:11 +0800

    20241113

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9915b6bb934997c74fedd336408c6490f6cd1855")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc117a0be5ab3ecd07e950031449553716c22cb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
